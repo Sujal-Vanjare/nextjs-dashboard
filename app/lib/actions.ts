9333c3263c5220afc49fabbd28e4f4b0445e549e
Sujal Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2025-06-08 17:10:18 +0530

    added ability to Create Invoice
    Using next.js server functions

---
+++ app/lib/actions.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import postgres from "postgres";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+// 👆 Connects to the PostgreSQL database with SSL encryption required for secure data transfer
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+// 👆 This creates a new schema by removing 'id' and 'date' from FormSchema
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+  // 👆 Get today's date in "YYYY-MM-DD" format by converting to ISO string and removing the time part
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
