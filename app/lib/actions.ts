49db8d9f420fd285de19a0f27502156df990a907
Sujal Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2025-06-08 17:48:43 +0530

    added ability to Update and Delete Invoice
    with next.js server actions, redirect & revalidate page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,6 +16,8 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
+// Create //
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 // 👆 This creates a new schema by removing 'id' and 'date' from FormSchema
 
@@ -35,4 +37,35 @@ export async function createInvoice(formData: FormData) {
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
+}
+
+// Update //
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+// Delete //
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
 }
